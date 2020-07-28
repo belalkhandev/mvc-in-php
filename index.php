aaab544789cd089a00fb8e93cc6e408ab5e65b5a
Belal Khan <belalkhan.dev@gmail.com>
2020-07-28 11:28:33 +0600

    add htaccess and bootstrap of the mvc

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-Welcome to MVC Framework in PHP
+<?php
+    session_start();
+
+    define('DS', DIRECTORY_SEPARATOR);
+    define("ROOT", dirname(__FILE__));
+
+    $url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];
+
+    require_once(ROOT . DS . 'core/' . DS . 'bootstrap.php');
